cd30cffc5e65680bc7a75abbe926d6e14b983ce1
SamuelTobon <79023630+SamuelTobon@users.noreply.github.com>
2024-09-21 20:19:26 -0500

    Edit Position FrontEnd

--- backEnd/src/main/java/com/adso/servicios/web/Repositorios/AdministradorRepository.java
+++ backEnd/src/main/java/com/adso/servicios/web/Repositorios/AdministradorRepository.java
@@ -1,6 +1,6 @@
 package com.adso.servicios.web.Repositorios;
 
-import java.util.List;
+//import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
